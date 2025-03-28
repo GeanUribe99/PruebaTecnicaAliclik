16e76486094cb71c375a7ae2463ed25a162b9651
GeanUribe99 <142021274+GeanUribe99@users.noreply.github.com>
2025-03-28 03:11:13 -0500

    feat: filtro de busqueda por nombre ususarios

--- src/componentes/ListUsuarios.jsx
+++ src/componentes/ListUsuarios.jsx
@@ -1,17 +1,47 @@
+import { useState,useEffect } from "react";
 import { Button } from "./ui/Buton";
 import { Table } from "./ui/Table";
+import { Input } from "./ui/Input";
+import { buscarUsuarioPorNombre } from "../services/usuarioservice";
 
-export function ListUsuarios({ listUsuarios, onEliminar }) {
-  console.log(listUsuarios)
+export function ListUsuarios({ listUsuarios, onEliminar, onEditar }) {
+
+  const [busqueda, setBusqueda] = useState("");
+  const [usuariosFiltrados, setUsuariosFiltrados] = useState(listUsuarios);
+
+  const handleBuscar = (e) => {
+    const valor = e.target.value;
+    setBusqueda(valor);
+    if (valor.trim() === "") {
+      setUsuariosFiltrados(listUsuarios);
+    } else {
+      const resultados = buscarUsuarioPorNombre(valor);
+      setUsuariosFiltrados(resultados);
+    }
+  };
+
+  // Si cambia la lista original, actualizar filtrados
+  useEffect(() => {
+    setUsuariosFiltrados(listUsuarios);
+  }, [listUsuarios]);
+
   return (
     <>
       <h2 className="my-3">Usuarios Registrados</h2>
-      {listUsuarios.length === 0 ? (
+      <Input
+        type="text"
+        placeholder="Buscar por nombre"
+        value={busqueda}
+        onChange={handleBuscar}
+        className="mb-3"
+      />
+
+      {usuariosFiltrados.length === 0 ? (
         <h5>No hay usuarios registrados.</h5>
       ) : (
         <Table headers={["#", "Imagen", "Nombre", "Apellido", "Teléfono", "Acciones"]}>
-          {listUsuarios.map((usuario, index) => (
-            <tr key={index}>
+          {usuariosFiltrados.map((usuario, index) => (
+            <tr key={usuario.id}>
               <th scope="row">{index + 1}</th>
               <td>
                 <img
@@ -25,7 +55,7 @@ export function ListUsuarios({ listUsuarios, onEliminar }) {
               <td>{usuario.apellido}</td>
               <td>{usuario.telefono}</td>
               <td>
-                <Button className="btn btn-sm btn-warning me-2">
+                <Button className="btn btn-sm btn-warning me-2" onClick={() => onEditar(usuario)}>
                   Editar
                 </Button>
                 <Button className="btn btn-sm btn-danger" onClick={() => onEliminar(usuario.id)}>
